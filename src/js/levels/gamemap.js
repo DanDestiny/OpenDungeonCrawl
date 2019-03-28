5f8cae9bb72de12a912065c613c3df34545b4f3c
Daniel <DanDestiny98@gmail.com>
2019-03-28 20:36:13 +1100

    Organisation and Placeholder
    - Added more folders and empty js scripts

--- src/js/levels/gamemap.js
+++ src/js/levels/gamemap.js
@@ -2,7 +2,7 @@ export class GameMap{
     // TODO:
     // LOAD LEVEL JSON FILE
     // CREATE GRID THAT SATISFIES LEVEL
-    // Fill Grid with level data
+    // FILL GRID WITH LEVEL DATA
     constructor(levelJson){
         
     }
